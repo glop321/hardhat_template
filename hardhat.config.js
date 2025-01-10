4930813fcdfeb6388201806549649e82591a8805
Emilio Lopez <glop321@gmail.com>
2025-01-10 13:17:58 +0100

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,10 @@
+{
+  "devDependencies": {
+    "chai": "^4.3.6",
+    "ethereum-waffle": "^3.4.4",
+    "ethers": "^5.6.9",
+    "hardhat": "^2.22.17",
+    "hardhat-deploy": "^0.14.0",
+    "hardhat-ethers": "^1.0.1"
+  }
+}
