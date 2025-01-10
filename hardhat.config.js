f09469371a7712c06b59f298ed31b76a392f04a4
Emilio Lopez <glop321@gmail.com>
2025-01-10 13:21:27 +0100

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,5 +6,12 @@
     "hardhat": "^2.22.17",
     "hardhat-deploy": "^0.14.0",
     "hardhat-ethers": "^1.0.1"
+  },
+  "dependencies": {
+    "18": "^0.0.0",
+    "@uniswap/v3-core": "^1.0.1",
+    "@uniswap/v3-periphery": "^1.4.4",
+    "dotenv": "^16.4.7",
+    "node": "^22.13.0"
   }
 }
